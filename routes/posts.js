02a863f512ebf95988a55d1f57b3d41043c10ea2
Shamsadov <shamsadovkh@gmail.com>
2021-06-17 12:02:00 +0300

    Cделал модель поста и роуты поста

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,53 @@
 const { Router } = require('express')
+const posts = require('../models/Post')
 const router = Router()
 
-
+router.get('./posts', async (req, res)=>{
+  const allPosts = await posts.find();
+  res.json(allPosts)
+});
+router.get('/posts/:id', async (req, res) =>{
+  const postById = await posts.findById(req.params.id);
+  res.json(postById);
+});
+router.get('/categories/:id/posts', async (req, res) =>{
+  const categoriesPosts = await posts.find()
+  res.json(categoriesPosts);
+});
+router.post('/posts', async (req, res)=>{
+  try {
+    const post = await new posts({
+      title: req.body,
+      text: req.body,
+      date: req.body,
+      author: req.body,
+      categoriesId: req.body,
+    })
+    await post.save();
+    res.json('Пост добавлен')
+  }catch (e){
+    console.log(e.message)
+  }
+});
+router.delete('/posts/:id', async (req,res)=>{
+  try {
+    const deletePost = await posts.findById(req.params.id);
+    deletePost.delete()
+    res.json('Пост удален')
+  }catch (e){
+    console.log(e.message)
+  }
+});
+router.patch('/posts/:id', async (req, res) =>{
+  try {
+    await posts.updateOne(req.params.id, {
+      $set: {
+        ...req.body
+      }
+    })
+  }catch (e){
+    console.log(e.message)
+  }res.json("Пост изменен")
+})
 
 module.exports = router
